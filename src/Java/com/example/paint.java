23833d50a809c800bbca9593f2054ecb2b2fc9a6
Michael <michaelchung451@gmail.com>
2021-09-21 13:00:14 +0100

    test test test

--- src/Java/com/example/paint.java
+++ src/Java/com/example/paint.java
@@ -7,9 +7,24 @@ public class paint {
 
         Scanner user_input = new Scanner(System.in);
 
+        // Welcome message
+        System.out.println("Hello! Welcome to the Paint Calculator\nPlease follow the instructions below and enter values when prompted\n");
+
         // Volume of Paint in a can
         System.out.print("Enter the amount of paint in a can in m^3: ");
         float paintCan = user_input.nextFloat();
+
+        float canPrice = 0.0f;
+
+        // Writing an if/else statement for cost of a sinlge can of paint
+        if (paintCan > 0.1){
+            canPrice += 1.50;
+            System.out.println("This can of paint costs £"+canPrice);
+        }
+        else {
+            canPrice += 2.50;
+            System.out.println("This can of paint costs £" + canPrice);
+        }
 
         // Inputting the room length
         System.out.print("Enter room length in meters: ");
@@ -32,7 +47,7 @@ public class paint {
         float doorHeight = user_input.nextFloat();
 
         // What is the thickness of the layer of paint
-        System.out.print("Enter thickness of paint in meters: ");
+        System.out.print("Enter thickness of paint layer in meters: ");
         float paintThickness = user_input.nextFloat();
 
         // Inputting the number of windows
@@ -53,7 +68,23 @@ public class paint {
         int paintNeededInt = (int) paintNeeded;
 
         // Determining the number of cans needed
-        System.out.println("The number of cans of paint needed is: ");
-        System.out.println(paintNeededInt);
+        System.out.println("The number of cans of paint needed is: " + paintNeededInt);
+
+        switch (paintNeededInt){
+            case 1:
+                System.out.println("   You only need 1 can of paint!");
+                break;
+            case 2,3,4:
+                System.out.println("   You need a few cans of paint!");
+                break;
+            default:
+                System.out.println("   You need a lot of paint!");
+        }
+
+        // Total cost
+        float totalCost = canPrice*paintNeededInt;
+        System.out.print(" The total cost is £" + totalCost);
+
+        System.out.println("FINISHED");
     }
 }
